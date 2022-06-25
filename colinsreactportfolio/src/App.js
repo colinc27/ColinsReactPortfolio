d7543003645891241708dd4959ffc96fc5f6a62b
colinc27 <colinchristoph27@gmail.com>
2022-06-25 14:43:22 -0700

    Add to contact

--- colinsreactportfolio/src/App.js
+++ colinsreactportfolio/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
+import ContactForm from './components/Contact';
 // import About from './components/About';
 // import Gallery from './components/Gallery';
-// import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
